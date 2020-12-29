fc9194fc27119d48ede50e9aff4157150709da77
Newland-A <ajn252@gmail.com>
2020-12-29 11:00:36 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import { addItem } from  './actions/items';
 class App extends Component {
 
   handleOnClick() {
-    this.props.store.dispatch(addItem());
+    // this.props.store.dispatch(addItem());
+    this.props.addItem();
   }
 
   render() {
+    debugger
     return (
       <div className="App">
         <button onClick={(event) => this.handleOnClick(event)}>
@@ -21,10 +23,26 @@ class App extends Component {
   }
 };
 
-const mapStateToProps = (state) => {
-  return {
-    items: state.items
-  };
-};
+// const mapStateToProps = (state) => {
+//   return {
+//     items: state.items
+//   };
+// };
+// Code change: this new function takes in dispatch as an argument
+// It then returns an object that contains a function as a value!
+// Notice above in handleOnClick() that this function, addItem(),
+// is what is called, NOT the addItem action creator itself.
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     addItem: () => {
+//       dispatch(addItem())
+//     }
+//   };
+// };
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
+// this is the equilvalent of writing the above methods
+// export default connect(state => ({ items: state.items }), { addItem })(App);
+// Code change: no mapDispatchToProps function required!
 
-export default connect(mapStateToProps)(App);
+// or can be simplified even further because we wwould still have access to this.props.dispatch()
+export default connect(state => ({ items: state.items }))(App);
